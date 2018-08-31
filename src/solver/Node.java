577e9c0b70ab198cbb406022483cb529d5d21c1f
Erlend Strandkleiv <erlend.h.strandkleiv@gmail.com>
2018-08-31 23:38:24 +1000

    generated pseudocode

--- src/solver/Node.java
+++ src/solver/Node.java
@@ -1,17 +1,29 @@
 package solver;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
 public class Node {
 	
 private Point2D pos;
 
+private ArrayList<Node> edgeNodes;
+
 public Node(Point2D pos) {
 	this.pos = (Point2D) pos.clone();
+	this.edgeNodes = new ArrayList<>();
 }
 
 public Point2D getPos() {
 	return pos;
+}
+
+public void addEdge(Node node) {
+	edgeNodes.add(node);
+}
+
+public ArrayList<Node> getEdges(){
+	return edgeNodes;
 }
 
 public void setPos(Point2D pos) {
