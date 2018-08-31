ecdafa08297a67f3e9fab51d646f6f05a7963f9a
Erlend Strandkleiv <erlend.h.strandkleiv@gmail.com>
2018-08-31 22:10:26 +1000

    added some logic

--- src/solver/HelpNode.java
+++ src/solver/HelpNode.java
@@ -5,7 +5,7 @@ import java.awt.geom.Point2D;
 public class HelpNode extends Node {
 
 	public HelpNode(Point2D pos) {
-		super(pos);
+		super((Point2D) pos.clone());
 	}
 
 }
--- src/solver/Node.java
+++ src/solver/Node.java
@@ -3,10 +3,11 @@ package solver;
 import java.awt.geom.Point2D;
 
 public class Node {
+	
 private Point2D pos;
 
 public Node(Point2D pos) {
-	this.pos = pos;
+	this.pos = (Point2D) pos.clone();
 }
 
 public Point2D getPos() {
@@ -14,8 +15,11 @@ public Point2D getPos() {
 }
 
 public void setPos(Point2D pos) {
-	this.pos = pos;
+	this.pos = (Point2D) pos.clone();
 }
 
+public double calculateDistance(Node node) {
+	return this.pos.distance(node.getPos());
+}
 
 }
